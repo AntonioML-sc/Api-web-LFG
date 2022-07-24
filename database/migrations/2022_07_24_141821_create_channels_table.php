33f1796c7c1a7198cb8cdf58ed3ff1a2ae854107
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-24 16:33:12 +0200

    feat: channels table created

---
+++ database/migrations/2022_07_24_141821_create_channels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('game_id')->nullable(false);
+            $table->foreign('game_id')
+                ->references('id')
+                ->on('games')
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+};
