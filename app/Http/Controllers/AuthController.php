62a8e9fc3bb563596e140577925341baaa1825e7
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-26 21:07:40 +0200

    Refactor: improve register user query with try-catch and logs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,28 +16,48 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
+        try {
+
+            Log::info('Trying to register a new user');
+
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors()->toJson(), 400);
+            }
+
+            $user = User::create([
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'password' => bcrypt($request->password)
+            ]);
+
+            // by default, we assign the role 'user' to every new user
+            $user->roles()->attach(self::ROLE_USER);
+
+            $token = JWTAuth::fromUser($user);
+
+            Log::info('New user registered: '. $user->email);
+
+            return response()->json(compact('user', 'token'), 201);
+
+        } catch (\Exception $exception) {
+
+            Log::error("Error in registering new user: " . $exception->getMessage());
+
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Error in registering new user'
+                ],
+                500
+            );
         }
-
-        $user = User::create([
-            'name' => $request->get('name'),
-            'email' => $request->get('email'),
-            'password' => bcrypt($request->password)
-        ]);
-
-        // by default, we assign the role 'user' to every new user
-        $user->roles()->attach(self::ROLE_USER);
-
-        $token = JWTAuth::fromUser($user);
-
-        return response()->json(compact('user', 'token'), 201);
     }
 
     public function login(Request $request)
