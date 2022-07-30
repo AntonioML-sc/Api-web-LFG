cf9932d2a6cbbf130e5eb78c681905606a1d17df
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-30 18:02:22 +0200

    Refactor: make unique column title in table games

--- database/migrations/2022_07_24_140810_create_games_table.php
+++ database/migrations/2022_07_24_140810_create_games_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->uuid('user_id')->nullable(false);
             $table->foreign('user_id')
                 ->references('id')
