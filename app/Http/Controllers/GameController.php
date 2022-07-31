09c505c6146d0093972adeb79a4b316260db9062
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-31 17:26:27 +0200

    Refactor: now any superadmin can also edit or delete any game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -77,7 +78,7 @@ class GameController extends Controller
         try {
 
             Log::info('retrieving all games');
-            
+
             $games = Game::query()
                 ->leftJoin('channels', 'games.id', '=', 'channels.game_id')
                 ->select('games.id as id', 'games.title as title', DB::raw("count(channels.id) as channels"))
@@ -109,10 +110,13 @@ class GameController extends Controller
 
     public function updateGame(Request $request, $gameId)
     {
+        define('SUPER_ADMIN_ID_LOCAL', "a3c06730-7018-467d-8187-cef95f37224d");
+
         try {
 
             Log::info('Updating game');
 
+            // validate data
             $validator = Validator::make($request->all(), [
                 'title' => ['string', 'max:255', 'min:3', 'unique:games'],
                 'genre' => ['string', 'max:255'],
@@ -132,6 +136,7 @@ class GameController extends Controller
 
             $game = Game::query()->find($gameId);
 
+            // check if the game exists
             if (!$game) {
                 return response()->json(
                     [
@@ -142,18 +147,22 @@ class GameController extends Controller
                 );
             }
 
-            $user_id = auth()->user()->id;
+            $userId = auth()->user()->id;
+            $userIsSuperAdmin = User::find($userId)->roles->contains(SUPER_ADMIN_ID_LOCAL);
 
-            if ($game->user_id != $user_id) {
+            // only the user that registered the game (who must be also admin) can update it
+            // any superadmin can also update the game
+            if (($game->user_id != $userId) && !$userIsSuperAdmin) {
                 return response()->json(
                     [
                         'success' => false,
-                        'message' => 'This game register does not belong to you'
+                        'message' => 'This useris not allowed to update this game'
                     ],
                     Response::HTTP_UNAUTHORIZED
                 );
             }
 
+            // edit the game fields with the values provided in $request 
             $title = $request->input("title");
             $genre = $request->input("genre");
             $age = $request->input("age");
@@ -201,13 +210,16 @@ class GameController extends Controller
     }
 
     public function deleteGame($gameId)
-    {
+    {        
+        define('SUPER_ADMIN_ID_LOCAL2', "a3c06730-7018-467d-8187-cef95f37224d");
+
         try {
 
             Log::info('Trying to delete game');
 
             $game = Game::query()->find($gameId);
 
+            // Check if the game exists
             if (!$game) {
                 return response()->json(
                     [
@@ -218,13 +230,15 @@ class GameController extends Controller
                 );
             }
 
-            $user_id = auth()->user()->id;
+            $userId = auth()->user()->id;
+            $userIsSuperAdmin = User::find($userId)->roles->contains(SUPER_ADMIN_ID_LOCAL2);
 
-            if ($game->user_id != $user_id) {
+            // only the user that registered the game (who must be also an admin) or a superadmin can delete it
+            if (($game->user_id != $userId) && !$userIsSuperAdmin) {
                 return response()->json(
                     [
                         'success' => false,
-                        'message' => 'This game register does not belong to you'
+                        'message' => 'This user is not allowed to delete this game'
                     ],
                     Response::HTTP_UNAUTHORIZED
                 );
